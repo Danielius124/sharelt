1e3ec6a24ecd6a036bb8707ab9c8b5843c82829e
Danielius <danieliusmiciulis@gmail.com>
2020-06-28 13:27:44 +0300

    Tikrinama

--- src/main/java/com/share/sharelt/ShareltApplication.java
+++ src/main/java/com/share/sharelt/ShareltApplication.java
@@ -8,7 +8,7 @@ public class ShareltApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShareltApplication.class, args);
-		System.out.println("LABAS");
+		System.out.println("LABAsS");
 	}
 
 }
