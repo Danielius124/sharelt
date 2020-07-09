cbd6c10157aa842fcae81a8c9bcbe154ad40bc73
Linas <stankunaslin0@gmail.com>
2020-07-09 14:27:43 +0300

    Pradeti exceptionai v2

---
+++ src/main/java/com/share/sharelt/exception/ApiRequestException.java
@@ -0,0 +1,12 @@
+package com.share.sharelt.exception;
+
+public class ApiRequestException extends RuntimeException{
+
+    public ApiRequestException(String message) {
+        super(message);
+    }
+
+    public ApiRequestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
